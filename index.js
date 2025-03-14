88770b7978e20218830bc776cf07f3bf36a65a81
Thiago Correa <thiagocorrea200375@gmail.com>
2025-03-14 20:44:12 -0300

    finsihed project

---
+++ index.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll(".buttons button")
+
+function switchWords() {
+   buttons.forEach(button => {
+      button.addEventListener("click", (e) => {
+         buttons.forEach(otherButton => {
+            if (otherButton !== button) {
+               otherButton.textContent = "Não"
+            }
+         })
+
+         if (button.textContent === "Sim") {
+            goToAnswerPage()
+         }
+   
+         if (button.textContent === "Não") {
+            button.textContent = "Sim"
+         }
+      })
+   })
+}
+switchWords()
+
+function goToAnswerPage() {
+   window.location.href = "/answer.html"
+}
